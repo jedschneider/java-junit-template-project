a15f0d1d3f4dff217324633dba8091cf3ea2a9de
Jed Schneider <jed.schneider@modeset.com>
2013-09-23 07:01:59 -0400

    adding some example test cases

--- src/uk/co/placona/helloWorld/HelloWorld.java
+++ src/uk/co/placona/helloWorld/HelloWorld.java
@@ -1,9 +1,22 @@
 package uk.co.placona.helloWorld;
 
 public class HelloWorld {
+  private String str;
 
-	public String sayHello() {
-		return "Hello World";
-	}
+  public HelloWorld(String password){
+    str = password;
+  }
+
+  public Boolean passwordValid(){
+    return !matchesWords() && longEnough();
+  }
+
+  public Boolean matchesWords(){
+    return str.matches("test|book");
+  }
+
+  public Boolean longEnough(){
+    return str.length() >= 7;
+  }
 
 }
--- test/helloWorld/HelloWorldTest.java
+++ test/helloWorld/HelloWorldTest.java
@@ -7,9 +7,31 @@ import org.junit.Test;
 import uk.co.placona.helloWorld.HelloWorld;
 
 public class HelloWorldTest {
-	@Test
-	public void testHellowWorld(){
-		HelloWorld hello = new HelloWorld();
-		assertEquals("A test for Hello World String", "Hello World", hello.sayHello());
-	}
+  public static final String VALID_PASSWORD = "jeD3atsP@sta";
+
+  @Test
+  public void testpasswordValid(){
+    HelloWorld hello = new HelloWorld(VALID_PASSWORD);
+    assertEquals(true, hello.passwordValid());
+  }
+
+  @Test
+  public void testMatchesBook(){
+    HelloWorld hello = new HelloWorld("bookbook");
+    assertEquals(false, hello.matchesWords());
+  }
+
+  @Test
+  public void testMatchesTest(){
+    HelloWorld hello = new HelloWorld("testtest");
+    assertEquals(false, hello.matchesWords());
+  }
+
+  @Test
+  public void testMatchesLength(){
+    HelloWorld hello = new HelloWorld("");
+    assertEquals(false, hello.longEnough());
+    HelloWorld hello2 = new HelloWorld("1234567");
+    assertEquals(true, hello2.longEnough());
+  }
 }
